6e8beb4aaaf689beffef9ae11c179245fb1c62e3
Davhho-dev <Davhho@utexas.edu>
2023-05-27 10:45:33 -0500

    Create function to hover correct amount of spaces based on ship length and orientation of ship

--- src/grid.js
+++ src/grid.js
@@ -15,4 +15,31 @@ function renderGrid(el) {
     };
 };
 
-export {renderGrid};
+function gridSelector(el, length, index, event, dir) {
+    let parent = el.parentNode;
+    let firstChild = parent.childNodes[index - 1];
+    if(gridExist(el, length, dir)) {
+        for(let i = 0; i < length; i++) {
+            event === "in" ? firstChild.style.backgroundColor = "white" : firstChild.style.backgroundColor = "black"
+            if(dir === "hori") {
+                parent = parent.nextElementSibling;
+                if(i !== length - 1) firstChild = parent.childNodes[index - 1];
+            }else {
+                firstChild = firstChild.nextElementSibling;
+            }
+        }
+    }
+
+}
+
+function gridExist(el, length, dir) {
+    let node;
+    dir === "hori" ? node = el.parentNode : node = el;
+    for(let i = 1; i < length; i++) {
+        if(node.nextElementSibling === null) return false;
+        else node = node.nextElementSibling;
+    }
+    return true;
+}
+
+export {renderGrid, gridSelector};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
-import { renderGrid } from "./grid";
+import { renderGrid, gridSelector} from "./grid";
 
-renderGrid();
+const gameboard = document.querySelector(".gameboard");
+
+renderGrid(gameboard);
+
+const length = 4;
+const direction = "vert";
+
+const grid = document.querySelectorAll("[class*=sq");
+grid.forEach(sq => {
+    sq.addEventListener("mouseenter", (e) => {
+        let className = e.target.className;
+        let index; 
+        className.length === 6 ? index = className.substring(className.length - 2) : index = className.charAt(className.length - 1);
+        gridSelector(e.target, length, index, "in", direction);
+        
+    })
+    sq.addEventListener("mouseleave", (e) => {
+        let className = e.target.className;
+        let index;
+        className.length === 6 ? index = className.substring(className.length - 2) : index = className.charAt(className.length - 1);
+        gridSelector(e.target, length, index, "out", direction);
+    })
+})
 
